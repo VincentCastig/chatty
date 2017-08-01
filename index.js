ed961b1bef351cb0dab6020e11967c6118e91e85
VincentCastig <vincent.castig@gmail.com>
2017-08-01 14:32:38 -0500

    no black diamond yet

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var port = 3000
+var app = express()
 
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+const messages = []
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({messages:messages});
+});
+
+app.post('/messages', function(req, res, next) {
+  console.log(req.body)
+  messages.push({ message:req.body.message, time: new Date() });
+  res.status(200).json({ messages:messages})
+});
+
+
+
+
+
+
+
+app.listen(port, function(){
+  console.log(`Listening to ${port}`)
+})
